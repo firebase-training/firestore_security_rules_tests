261458e0fab3b6c4f9f8ed91b6ab18c42f819761
Yves Bonjour <yves.bonjour@ergon.ch>
2019-07-10 10:04:54 +0200

    Removes solution

--- test/security_rules.spec.ts
+++ test/security_rules.spec.ts
@@ -1,34 +1,13 @@
 /* tslint:disable */
 import * as firebase from '@firebase/testing'
-import {setup, teardown, testDatabase} from './helpers'
 
 describe('security rules', () => {
-    beforeEach(async () => {
-        const data = {
-            ['users/joe']: { name: 'joe' },
-            ['users/mary']: { name: 'mary' }
-        }
-        await setup(data)
-    });
-    afterEach(async () => {
-        await teardown()
+
+    it('allows users to read own documents', () => {
+        // TODO: Implement
     });
 
-    it('allows users to read own documents', async () => {
-        await firebase.assertSucceeds(
-            testDatabase('joe')
-                .collection('users')
-                .doc('joe')
-                .get()
-            );
-    });
-
-    it('denes users to read other documents', async () => {
-        await firebase.assertFails(
-            testDatabase('joe')
-                .collection('users')
-                .doc('mary')
-                .get()
-        );
+    it('denes users to read other documents', () => {
+        // TODO: Implement
     });
 });
